ba3c943637b80a8176f0064a76491a859db6b308
Dan-Yee <dyee3435@gmail.com>
2022-02-13 00:14:20 -0500

    Created physicalLayer.h and implemented physicalLayer.c

---
+++ Project 1/physicalLayer.c
@@ -0,0 +1,51 @@
+/**
+ * Implementation of the "Physical Layer"
+ * Implements functions for converting a char to binary string and parity checking
+ */
+#include "physicalLayer.h"
+
+/**
+ * Function that converts a character to its binary form
+ */
+char* charToBinary(const char character, char* result) {
+    char binaryStr[9] = "00000000";
+
+    for(int i = 7, j = 0; i >= 0 && j < 8; i--, j++)
+        if(character & (1 << i))
+            binaryStr[j] = '1';
+    binaryStr[9] = '\0';
+    strcpy(result, binaryStr);
+
+    return result;
+}
+
+/**
+ * Function that converts a binary string to int
+ */
+int binaryToInt(const char* string) {
+    return (int) strtol(string, NULL, 2);
+}
+
+/**
+ * Function that flips the parity bit on a binary string with even parity
+ */
+char* addParityBit(char* string) {
+    int bitCount = 0;
+
+    for(int i = 0; i < strlen(string); i++)
+        if(string[i] == '1')
+            bitCount++;
+    if(bitCount % 2 == 0)
+        string[0] = '1';
+
+    return string;
+}
+
+/**
+ * Function that removes the odd parity bit if needed
+ */
+char* removeParityBit(char* string) {
+    if(string[0] == '1')
+        string[0] = '0';
+    return string;
+}
---
+++ Project 1/physicalLayer.h
@@ -0,0 +1,31 @@
+/**
+ * Specifications on the implementation of the physical layer
+ * Specifies functions for converting char to binary and binary to int
+ */
+#include <string.h>
+#include <stdlib.h>
+
+#ifndef PHYSICAL_HEADER
+#define PHYSICAL_HEADER
+
+/**
+ * Function to convert a char to a 8-bit binary string
+ */
+char* charToBinary(const char, char*);
+
+/**
+ * Function to convert a binary string to an int
+ */
+int binaryToInt(const char*);
+
+/**
+ * Function to check the parity and add a parity bit if necessary
+ */
+char* addParityBit(char*);
+
+/**
+ * Function to remove the odd parity bit if necessary
+ */
+char* removeParityBit(char*);
+
+#endif
