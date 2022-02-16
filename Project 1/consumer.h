1ce0bbac0dfa08cb7038726bf6930180a6fd50cf
Dan-Yee <dyee3435@gmail.com>
2022-02-15 22:32:17 -0500

    updated header files

---
+++ Project 1/consumer.h
@@ -0,0 +1,16 @@
+/**
+ * Specifications on the implementation of the Consumer (Receiver)
+ */
+#include "dataFrame.h"
+#include "data.h"
+#include <stdio.h>
+
+#ifndef CONSUMER_HEADER
+#define CONSUMER_HEADER
+
+/**
+ * Function to start the consumer after the Producer has finished "transmitting" data
+ */
+void consumer(void);
+
+#endif
