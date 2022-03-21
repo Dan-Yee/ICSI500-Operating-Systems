8ad617b51a0900d02df8e1ad5c163160fe8d0135
Dan-Yee <dyee3435@gmail.com>
2022-03-21 18:40:10 -0400

    split CRC and Hamming Code into two modules

---
+++ Project 3/hamming.c
@@ -0,0 +1,4 @@
+/**
+ * Implementation of functions used to perform Hamming code error detection and single-bit error correction
+ */
+#include "hamming.h"
---
+++ Project 3/hamming.h
@@ -0,0 +1,14 @@
+/**
+ * Specifications on the implementating Hamming code error detection and single-bit error correction
+ */
+#include "dataFrame.h"
+
+#ifndef HAMMING_HEADER
+#define HAMMING_HEADER
+
+/**
+ * Function that calculates the Hamming code of a dataframe and perform single-bit error correction if needed
+ */
+void hamming(struct dataFrame*);
+
+#endif
